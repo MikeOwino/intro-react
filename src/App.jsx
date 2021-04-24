ec9b7c2a36714e5f55fc17e8675e1717965ef415
Mike Owino <mike@mikeowino.com>
2021-04-24 18:14:03 +0000

    uncomment grades section

--- src/App.jsx
+++ src/App.jsx
@@ -81,7 +81,7 @@ addStudent(studentName) {
     }
 
     /* Uncomment lines below to render grades*/
-    /*if (this.state.buttonClicked === "grades") {
+    if (this.state.buttonClicked === "grades") {
       tabChoice = (
         <Table
           tableNames={this.state.assignments}
@@ -90,7 +90,7 @@ addStudent(studentName) {
           data={this.state.grades}
         />
       );
-    }*/
+    }
 
     return (
       <div>
